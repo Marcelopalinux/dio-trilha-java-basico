c154644395fa2ac03a9ed22af6ba02a09ae61a00
Marcelo Andrade <marcelopa.linux@gmail.com>
2024-08-20 21:26:16 -0300

    salvando projeto

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+
+public class ParametrosInvalidosException extends Exception{
+	
+	public ParametrosInvalidosException(String Mensagem) {
+		// TODO Auto-generated constructor stub
+		super(Mensagem);
+	}
+
+}
